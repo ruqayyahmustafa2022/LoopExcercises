9f52b5db449b3ba60260d4db733895d70344c701
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-02-14 14:48:10 -0600

    new project added LoopExcercies

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Exercise 2.6
+	(1)
+
+
+	 */
+        //Declare the variables
+        int theNumber = 5;
+        String newInput;
+        int temp = 0;
+
+    for(int i = 1; i <= theNumber; i++) {
+       temp = i * (temp + i);
+       if(temp % i == 0 &&
+
+
+    }
+
+    }
+}
